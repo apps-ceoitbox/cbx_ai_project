6369b6326b939316d3ea66372b4afb1ac559e6ed
Raghbir Singh <rkgaming0203@gmail.com>
2025-04-11 20:51:35 +0530

    build

--- backend/src/utils/AI.ts
+++ backend/src/utils/AI.ts
@@ -15,8 +15,7 @@ export class AI {
     constructor(apiProvider: ApiProvider) {
         this.apiProvider = apiProvider;
         this.ai = new OpenAI({
-            apiKey: "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",
-            // apiKey: apiProvider.apiKey,
+            apiKey: apiProvider.apiKey,
         });
     }
 
@@ -24,7 +23,7 @@ export class AI {
         switch (this.apiProvider.name) {
             case "ChatGPT (OpenAI)": {
                 const response = await this.ai.chat.completions.create({
-                    model: "gpt-4-turbo",
+                    model: this.apiProvider.model,
                     messages: [{ role: "user", content: prompt }],
                     temperature: this.apiProvider.temperature,
                     max_tokens: this.apiProvider.maxTokens,
