fa6b07e1883096a836acad2cd71c4c7f2881c50f
Raghbir Singh <rkgaming0203@gmail.com>
2025-04-12 14:55:07 +0530

    build

---
+++ backend/src/controllers/submissions.controller.ts
@@ -0,0 +1,25 @@
+import dotenv from "dotenv";
+import { HttpStatusCodes } from "../utils/errorCodes";
+import Submission, { SubmissionInterface } from "../models/submission.model";
+import { asyncHandler } from "../utils/asyncHandler";
+dotenv.config();
+
+export default class SubmissionsController {
+
+    static async getSubmissions(req, res) {
+        const submissions = await Submission.find();
+        res.status(HttpStatusCodes.OK).json({ message: 'Submissions fetched successfully', data: submissions });
+    }
+
+    static async getUserSubmission(req, res) {
+        const submission = await Submission.find({ email: req.user.email });
+        res.status(HttpStatusCodes.OK).json({ message: 'Submission fetched successfully', data: submission });
+    }
+
+    static async updateSubmission(req, res) {
+        const id = req.params.id;
+        const submission = await Submission.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(HttpStatusCodes.OK).json({ message: 'Submission updated successfully', data: submission });
+    }
+
+}
---
+++ backend/src/models/submission.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+
+
+export interface SubmissionInterface {
+    _id: string;
+    name: string;
+    email: string;
+    company: string;
+    tool: string;
+    date: string;
+    status: string;
+    apiUsed: string;
+    questionsAndAnswers: Record<string, string>;
+    generatedContent: {
+        title: string;
+        sections: {
+            title: string;
+            content: string;
+        }[];
+    };
+    createdAt: Date;
+    updatedAt: Date;
+}
+const submissionSchema: Schema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    company: { type: String, required: true },
+    tool: { type: String, required: true },
+    date: { type: Date, required: true },
+    status: { type: String, required: true, default: "Completed" },
+    apiUsed: { type: String, required: true, default: "" },
+    questionsAndAnswers: { type: Object, required: true, default: {} },
+    generatedContent: { type: Object, required: true },
+}, { timestamps: true });
+
+const Submission = mongoose.model<SubmissionInterface & Document>("submission", submissionSchema);
+export default Submission;
